f35e9333588f8dd120d815fecb0d8344f0bb40d9
Sinis2101 <sinisterra2101@gmail.com>
2021-02-12 15:04:46 -0500

    UserAccount Class created with attributes, constructor and getters.

---
+++ src/model/Browser.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Browser {
+
+    Firefox, Edge, Chrome, Opera, Vivaldi;
+
+}
--- src/model/UserAccount.java
+++ src/model/UserAccount.java
@@ -2,6 +2,47 @@ package model;
 
 public class UserAccount {
 
+    private String username;
+    private String password;
+    private String profilePic;
+    private Genre genre;
+    private String career;
+    private String birthday;
+    private Browser favBrowser;
 
+    public UserAccount(String username, String password, String profilePic, Genre genre, String career, String birthday, Browser favBrowser) {
 
+        this.username = username;
+        this.password = password;
+        this.profilePic = profilePic;
+        this.genre = genre;
+        this.career = career;
+        this.birthday = birthday;
+        this.favBrowser = favBrowser;
+
+    }
+
+    // GETTERS
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getProfilePic() {
+        return profilePic;
+    }
+    public Genre getGenre() {
+        return genre;
+    }
+    public String getCareer() {
+        return career;
+    }
+    public String getBirthday() {
+        return birthday;
+    }
+    public Browser getFavBrowser() {
+        return favBrowser;
+    }
 }
